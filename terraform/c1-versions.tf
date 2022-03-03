04f46205df2d968378bc170a364b90d8e7c4ae52
Phillip <Phillip@sarangsuniljoshigmail.onmicrosoft.com>
2022-03-03 17:53:25 +0000

    runtime

--- terraform/c1-versions.tf
+++ terraform/c1-versions.tf
@@ -18,15 +18,23 @@ terraform {
   }
 }
 
-# resource "azurerm_resource_group" "myrg" {
-#   name = var.resource_group_name
-#   location = var.location
-# }
-
 resource "azurerm_data_factory" "mydatafactory" {
   name                = "mydatafactory123466898708"
   location            = var.location
   resource_group_name = var.resource_group_name
 }
+
+
+
+
+resource "azurerm_" "myruntime" {
+  name                = "data_factory_integration_runtime_azure_ssis_5564747476476457"
+  data_factory_id     = azurerm_data_factory.mydatafactory.id
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  node_size = "Standard_D8_v3"
+}
+
 
 
